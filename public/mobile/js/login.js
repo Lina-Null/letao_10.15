afdfdf37299a5d48c0dfbfaef741a6580d33a6ca
unknown <lina0709521@icloud.com>
2019-11-11 18:44:47 +0800

    登录功能完成

---
+++ public/mobile/js/login.js
@@ -0,0 +1,42 @@
+$(function () {
+
+    $('#loginBtn').click(function(){
+        var username = $('#username').val().trim();
+        var password = $('#password').val().trim();
+        if(username === ""){
+            mui.toast("请输入用户名");
+            return;
+        }
+        if(password === ""){
+            mui.toast("请输入密码");
+            return;
+        }
+        $.ajax({
+            url:" /user/login",
+            type:"post",
+            dataType:"json",
+            data:{
+                username:username,
+                password:password
+            },
+            success:function (info) {
+                console.log(info);
+                if (info.error === 403 ){
+                    mui.toast("用户名或密码错误");
+                    return;
+                }
+                //登录成功
+                //1、如果是从其他页面拦截过来的，调回去
+                //2、如果是直接访问login.html,跳转到个人中心页
+                if(location.search.indexOf("retUrl") > -1 ){
+                        var retUrl = location.search.replace("?retUrl=","");
+                        console.log(retUrl);
+                        location.href = retUrl ;
+                }else{
+                    location.href = "user.html";
+                }
+            }
+        });
+    });
+
+})
